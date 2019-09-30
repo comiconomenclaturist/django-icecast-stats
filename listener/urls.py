c8298f67e698850507f976cf493c63d7f364d01a
comiconomenclaturist <james@4thharmonic.com>
2019-09-30 14:35:01 +0100

    AggregateView removed

--- listener/urls.py
+++ listener/urls.py
@@ -5,7 +5,6 @@ app_name = 'listener'
 
 # app_name will help us do a reverse look-up later.
 urlpatterns = [
-    path('aggregate/', AggregateView.as_view()),
     path('count/', CountViewSet.as_view({'get': 'list'})),
     path('count2/', Count2ViewSet.as_view()),
     path('hours/', HoursViewSet.as_view({'get': 'list'})),
