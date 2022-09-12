0aa1924507a4fe8d628375857d74bbf0b595ccf6
comiconomenclaturist <james@4thharmonic.com>
2022-09-12 19:12:28 +0100

    Mostly JS updates. Tooltips fixed. Legend still needs fixing. CSS added to login page

--- base/static/js/global.js
+++ base/static/js/global.js
@@ -1,6 +1,3 @@
-Chart.defaults.scale.grid.color = '#555';
-Chart.defaults.scale.grid.borderColor = '#555';
-Chart.defaults.scale.grid.drawOnChartArea = false;
 Chart.defaults.plugins.legend.position = 'right';
 Chart.defaults.plugins.legend.labels.boxWidth = 12;
 Chart.defaults.plugins.title.display = true;
@@ -10,5 +7,11 @@ Chart.defaults.plugins.title.font = {
     family: "'Open Sans', sans-serif",
 }
 Chart.defaults.color = 'lightgrey';
+Chart.defaults.scale.grid.color = '#555';
+Chart.defaults.scale.grid.borderColor = '#555';
+Chart.defaults.scale.grid.drawOnChartArea = false;
+Chart.defaults.scale.ticks.font = {
+    size: 10,
+}
 
 var colours = ['#FF4E67', '#F77D50', '#EFBD52', '#DAE753', '#9ADF55', '#62D856', '#58D07E', '#59C9AB', '#5AB3C1']
