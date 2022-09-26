a6c5eab07dec8e5389dbec59fc9994a127568dd9
comiconomenclaturist <james@4thharmonic.com>
2022-09-26 13:06:49 +0100

    Fixed mobile size issue. Fixed the issue where if a user is logged out due to a timeout, then redirect to login with the same search parameters

--- stats/middleware.py
+++ stats/middleware.py
@@ -1,4 +1,5 @@
 from django.shortcuts import HttpResponseRedirect
+from django.http import JsonResponse
 from urllib.parse import quote
 
 
@@ -9,6 +10,14 @@ class AuthRequiredMiddleware(object):
     def __call__(self, request):
         # Code to be executed for each request before the view (and later middleware) are called.
         if not request.user.is_authenticated and request.path != "/login/":
+            if request.headers.get("x-requested-with") == "XMLHttpRequest":
+                return JsonResponse(
+                    {
+                        "status": "false",
+                        "message": "You don't have permission to access this resource",
+                    },
+                    status=403,
+                )
             path = quote(request.get_full_path())
             return HttpResponseRedirect(f"/login/?next={path}")
 
